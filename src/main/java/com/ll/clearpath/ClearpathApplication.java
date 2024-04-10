30e315359f0de435c1a8096d46e19127e20fee19
UJIN901 <dalvit9655@gmail.com>
2024-04-10 15:50:31 +0900

    chore: init project

---
+++ src/main/java/com/ll/clearpath/ClearpathApplication.java
@@ -0,0 +1,13 @@
+package com.ll.clearpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearpathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearpathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/clearpath/ClearpathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.clearpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearpathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

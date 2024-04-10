19093aa17a1641ad822967af2c5cfdb09b9db807
UJIN901 <dalvit9655@gmail.com>
2024-04-10 16:10:40 +0900

    feat: SecurityConfig 추가

---
+++ src/main/java/com/ll/clearpath/global/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.ll.clearpath.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/h2-console/**")
+                                .permitAll()
+                                .anyRequest()
+                                .permitAll()
+                )
+                .headers(
+                        headers ->
+                                headers.frameOptions(
+                                        frameOptions ->
+                                                frameOptions.sameOrigin()
+                                )
+                )
+                .csrf(
+                        csrf ->
+                                csrf.ignoringRequestMatchers(
+                                        "/h2-console/**"
+                                )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

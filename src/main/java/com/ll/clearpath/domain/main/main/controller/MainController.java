75cd16e0e3e188c6622c5434d4c9cc79ae24ce50
UJIN901 <dalvit9655@gmail.com>
2024-05-20 17:22:08 +0900

    feat: kakao 지도 생성 - 현재 위치 중심

--- src/main/java/com/ll/clearpath/domain/main/main/controller/MainController.java
+++ src/main/java/com/ll/clearpath/domain/main/main/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.ll.clearpath.domain.main.main.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 //@PreAuthorize("isAuthenticated()")
 public class MainController {
+    @Value("${maps.kakao.javascript-key}")
+    private String kakaoMapsApiKey;
 
     @GetMapping("/")
     public String showMain(Model model){
         return "domain/main/main/main";
+    }
+
+    @GetMapping("/map")
+    public String showMap(Model model){
+        model.addAttribute("kakaoMapsApiKey", kakaoMapsApiKey);
+        return "domain/main/main/map";
     }
 }
